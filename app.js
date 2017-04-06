b66306197ecb115ddcff5ffaa975ef8f32668844
htwright <harrisontwright@gmail.com>
2017-04-06 11:33:33 -0500

    working cats

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-$('.thumbnails').click(function(event) {
-  console.log(event.target);
-  //$('.hero').find('img').remove();
-  //event.stopPropagation();
-  let clicked = $(this).find('img').attr('src');
-  $('.hero').find('img').attr(clicked);
-});
+//$('.thumbnails').click(function(event) {
+//  console.log(event.target);
+//  //$('.hero').find('img').remove();
+//  //event.stopPropagation();
+//  let clicked = $(this).find('img').clone();
+//  $('.hero').find('img').replaceWith(clicked);
+//});
+//
+////use src retrieve attr from thumbnail and 
+////change attr for hero
 
-//use src retrieve attr from thumbnail and 
-//change attr for hero
+$('.thumbnails').click(function(event) {
+  let clicked = $(event.target).attr('src');
+  $('.hero').find('img').attr('src', clicked);
+});
