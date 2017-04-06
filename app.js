d624470064ea84ace468fc401c96be3fe5b9be3f
pham-thien <pham.thien@outlook.com>
2017-04-06 14:11:08 -0400

    add folders

--- app.js
+++
@@ -1,9 +0,0 @@
-//
-//$('.thumbnails').click(function(event) {
-//  let clicked = $(event.target).attr('src');
-//  $('.hero').find('img').attr('src', clicked);
-//});
-$('submit').on('click', function(event){
-  let inputs = $('#number-chooser :input');
-  console.log(inputs); 
-})
