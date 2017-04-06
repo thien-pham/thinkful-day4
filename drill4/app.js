cbb85c7cd2ae2edfdcac0b2ebcb8da81ab50b7c0
pham-thien <pham.thien@outlook.com>
2017-04-06 15:44:16 -0400

    add unique wc

--- drill4/app.js
+++ drill4/app.js
@@ -1,5 +1,4 @@
 //In this challenge, you'll implement a small web app that runs basic text analytics on user submitted text. We provide initial HTML and CSS files, and you'll be responsible for adding CSS classes to the HTML for your JavaScript code to hook into, and for writing JavaScript code that computes and displays 3 metrics:
-//
 
 // take submission as string
 // split it at the spaces array.split(' ')
@@ -21,20 +20,20 @@
 //Average word length in characters of the submitted text
 //The program should print each of these metrics in the appropriate area in the results section.
 
+
+
 function uniqueCount(input){
-  let results = [];
+  let results = {};
   let inputArr= input.split(' ');
   for (let i=0;i<=inputArr.length;i++){
     if (!(inputArr[i] in results)){
-      results.push(inputArr[i]);
+      results[inputArr[i]] = 1;
     } else {
-      continue;
+      results[inputArr[i]]++;
     }
   }
-  return results.length;
+  return Object.keys(results).length;
 }
-
-
 
 function wordCount(input){
   let inputArr = [];
