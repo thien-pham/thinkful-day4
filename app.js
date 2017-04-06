87541f76844a9aea44b29655ab9324a748b11133
htwright <harrisontwright@gmail.com>
2017-04-06 13:28:07 -0500

    fizzbuzz done

--- app.js
+++ app.js
@@ -3,7 +3,23 @@
 //  let clicked = $(event.target).attr('src');
 //  $('.hero').find('img').attr('src', clicked);
 //});
-$('submit').on('click', function(event){
-  let inputs = $('#number-chooser :input');
-  console.log(inputs); 
+$('#number-chooser').submit(function(event){
+  event.preventDefault();
+  let input = $('#number-choice').val();
+  console.log(input);
+  let results = [];
+  for (i=1;i<=input;i++){
+    if(i%15===0){
+      results.push('fizzbuzz');
+    } else if (i%5===0) {
+      results.push('buzz');
+    } else if (i%3===0){
+      results.push('fizz');
+    } else {
+      results.push(i);
+    }
+  }
+  results.forEach(function(result){
+     $('.js-results').append(`<div class="fizz-buzz-item"><span>${result}</span></div>`);
+  })
 })
