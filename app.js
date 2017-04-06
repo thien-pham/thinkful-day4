21882349a568a0bc689872666848d795c80cf1ff
htwright <harrisontwright@gmail.com>
2017-04-06 12:34:13 -0500

    second drill start

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-//$('.thumbnails').click(function(event) {
-//  console.log(event.target);
-//  //$('.hero').find('img').remove();
-//  //event.stopPropagation();
-//  let clicked = $(this).find('img').clone();
-//  $('.hero').find('img').replaceWith(clicked);
-//});
 //
-////use src retrieve attr from thumbnail and 
-////change attr for hero
-
-$('.thumbnails').click(function(event) {
-  let clicked = $(event.target).attr('src');
-  $('.hero').find('img').attr('src', clicked);
-});
+//$('.thumbnails').click(function(event) {
+//  let clicked = $(event.target).attr('src');
+//  $('.hero').find('img').attr('src', clicked);
+//});
+$('submit').on('click', function(event){
+  let inputs = $('#number-chooser :input');
+  console.log(inputs); 
+})
