7fbda456e6e6ce066af65d3c7496790f1f5251db
htwright <harrisontwright@gmail.com>
2017-04-06 22:11:58 -0500

    fixed fizzbuzz shouts out to dakota

--- drill2/app.js
+++ drill2/app.js
@@ -1,19 +1,27 @@
 $('#number-chooser').submit(function(event){
   event.preventDefault();
+  $('.js-results').empty();
   let input = $('#number-choice').val();
     console.log(input);
+    let newClass = '';
 
-  for (i=1;i<input;i++){
+  for (i=1;i<=input;i++){
     if(i%15===0){
       result='fizzbuzz';
+      newClass='fizzbuzz';
     } else if (i%5===0) {
       result='buzz';
+      newClass='buzz';
     } else if (i%3===0){
       result='fizz';
+      newClass='fizz';
     } else {
       result =i;
     }
-    $(.js-results).append(`<div class="fizz-buzz-item"><span>${result}</span></div>`) 
-
+    if (result=='fizz'||result=='buzz'||result=='fizzbuzz'){
+      $('.js-results').append(`<div class="fizz-buzz-item ${newClass}"><span>${result}</span></div>`)
+    } else {
+      $('.js-results').append(`<div class="fizz-buzz-item"><span>${result}</span></div>`) 
+    } 
   }
 })
--- drill4/app.js
+++ drill4/app.js
@@ -1,28 +1,3 @@
-//In this challenge, you'll implement a small web app that runs basic text analytics on user submitted text. We provide initial HTML and CSS files, and you'll be responsible for adding CSS classes to the HTML for your JavaScript code to hook into, and for writing JavaScript code that computes and displays 3 metrics:
-
-// take submission as string
-// split it at the spaces array.split(' ')
-//
-//Total word count of the submitted text
-//function array.length
-
-
-
-//Unique word count of the submitted text
-//for loop iteration through the array
-//if word is already in the new array, do nothing
-//if (words[i] in wordFrequencies)
-//wordFrequencies[words[i]]=1; push the actual word into the array as a key
-//if we haven't seen the word before, push it to a new array
-
-
-
-//Average word length in characters of the submitted text
-
-//The program should print each of these metrics in the appropriate area in the results section.
-
-
-
 function averageLength(input1){
   let results1 = [];
   let inputs = input1.split(' ');
@@ -34,7 +9,6 @@ function averageLength(input1){
   return j / x;
   
 }
-
 function uniqueCount(input2){
   let results2 = {};
   let inputArr = input2.split(' ');
@@ -47,7 +21,6 @@ function uniqueCount(input2){
   }
   return Object.keys(results2).length;
 }
-
 function wordCount(input3){
   let inputArr = [];
   inputArr = input3.split(' ');
